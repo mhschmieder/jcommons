1b23835f35bfb96b282b8903fff82742e97d507e
Mark Schmieder <markhschmieder@gmail.com>
2026-06-17 23:47:27 -0400

    Forgot to add module.info for jcommons after bumping past Java 8, and in the process discovered that RtfUtilities depends on Swing so that class got moved to the jgui library.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module jcommons {
+    exports com.mhschmieder.jcommons.branding;
+    exports com.mhschmieder.jcommons.collections;
+    exports com.mhschmieder.jcommons.concurrent;
+    exports com.mhschmieder.jcommons.html;
+    exports com.mhschmieder.jcommons.io;
+    exports com.mhschmieder.jcommons.lang;
+    exports com.mhschmieder.jcommons.net;
+    exports com.mhschmieder.jcommons.random;
+    exports com.mhschmieder.jcommons.security;
+    exports com.mhschmieder.jcommons.text;
+    exports com.mhschmieder.jcommons.time;
+    exports com.mhschmieder.jcommons.util;
+    exports com.mhschmieder.jcommons.xml;
+    requires org.apache.commons.io;
+    requires org.apache.commons.rng.api;
+    requires java.logging;
+    requires java.management;
+    requires commons.math3;
+    requires org.apache.commons.lang3;
+    requires java.prefs;
+    requires java.xml;
+}
